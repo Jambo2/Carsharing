354bc08115a67b6313b0cb753ff52172a1bd7b3f
Egor <egor.zaytsev205@list.ru>
2025-02-03 12:20:31 +0300

    Carsharing

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../app/styles/Header.css'
+import Menu from './menu/Menu'
+
+export default function Header() {
+  return (
+    <>
+     <header className='header'>
+        <Menu/>
+     </header>
+    </>
+  )
+}
---
+++ src/components/header/menu/Menu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../../app/styles/Header.css'
+import 'boxicons'
+import {Link} from 'react-router-dom'
+
+export default function 
+() {
+  return (
+    <>
+        <div className='block__menu'>
+            <div className="logo">
+                <Link to='/'>
+                    <box-icon type='solid' name='car' color='white' size='4em'></box-icon>
+                </Link>    
+            </div> 
+            <nav>
+              <ul className='menu__list'>
+                <li><Link href="">Каршеринг</Link></li>
+                <li><Link href="">Подписка</Link></li>
+                <li><Link href="">Для бизнеса</Link></li>
+              </ul>
+            </nav>       
+        </div>
+    </>
+  )
+}
